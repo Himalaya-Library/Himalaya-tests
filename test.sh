6f889f857e6375bc6566076c2d3ff6d79916d746
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2018-12-17 10:54:01 +0100

    clearer log messages

--- test.sh
+++ test.sh
@@ -11,15 +11,15 @@ for t in ${tests}
 do
     count=$(($count + 1))
 
-    cd "$t" && ./test
-
-    [ "x$?" != "x0" ] && {
+    printf "Executing: %s ... " "$t"
+    ( cd "$t" && ./test > test.log 2>&1 ) && printf "OK" || {
         errors=$(($errors + 1))
-        printf "Test failed: %s\n" "$t"
+        printf "FAILED"
     }
+    printf "\n"
 done 
 
-printf "--------------------------\n"
+printf -- "--------------------------\n"
 printf "Number of passed tests: %s\n" $(($count - $errors))
 printf "Number of failed tests: %s\n" $(($errors))
-printf "--------------------------\n"
+printf -- "--------------------------\n"
